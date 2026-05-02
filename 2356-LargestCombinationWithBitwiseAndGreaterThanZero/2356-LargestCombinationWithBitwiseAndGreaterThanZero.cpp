db6625aa1b5772f3fafbef62a6e66b4822c110ce
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:36:43 +0530

    Create 2356-LargestCombinationWithBitwiseAndGreaterThanZero

---
+++ 2356-LargestCombinationWithBitwiseAndGreaterThanZero/2356-LargestCombinationWithBitwiseAndGreaterThanZero.cpp
@@ -0,0 +1,36 @@
+// Last updated: 02/05/2026, 22:36:42
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+
+        vector<int> ans(32, 0);
+
+        for(int x : candidates){
+            find(x, ans);
+            
+        }
+
+        int res = 0;
+        for(int i = 0; i<32; i++){
+            res = max(res, ans[i]);
+        }
+        
+        return res;
+    }
+
+    void find(int n, vector<int>& ans){
+
+        int j = 31;
+
+        while(n > 0){
+
+            int a = n&1;
+
+            ans[j] += a;
+
+            n >>= 1;
+
+            j--;
+        }
+    }
+};
