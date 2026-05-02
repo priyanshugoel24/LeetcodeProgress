f6c1660a772d293c49904a019a3f82df167695c1
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:40:40 +0530

    Create 1371-MinimumRemoveToMakeValidParentheses

---
+++ 1371-MinimumRemoveToMakeValidParentheses/1371-MinimumRemoveToMakeValidParentheses.cpp
@@ -0,0 +1,46 @@
+// Last updated: 02/05/2026, 22:40:40
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        
+        int n = s.length();
+
+        string result = "";
+
+        // iterate from left to right -> eliminate extra close brackets
+        int open = 0;
+        for(int i = 0; i < n; i++){
+
+            if(s[i] >= 'a' && s[i] <= 'z'){
+                result.push_back(s[i]);
+            }else if(s[i] == '('){
+                open++;
+                result.push_back(s[i]);
+            }else if(open > 0){
+                open--;
+                result.push_back(s[i]);
+            }
+        }
+
+        // iterate from right to left -> eliminate extra open brackets
+        string final_result = "";
+
+        int close = 0;
+
+        for(int i = result.length() - 1; i >= 0; i--){
+            if(result[i] >= 'a' && result[i] <= 'z'){
+                final_result.push_back(result[i]);
+            }else if(result[i] == ')'){
+                close++;
+                final_result.push_back(result[i]);
+            }else if(close > 0){
+                close--;
+                final_result.push_back(result[i]);
+            }
+        }
+
+        reverse(begin(final_result), end(final_result));
+
+        return final_result;
+    }
+};
