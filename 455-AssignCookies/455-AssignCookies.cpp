151640f2180cbf1b820630ec64e84861222e5773
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:44:43 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.cpp
@@ -0,0 +1,22 @@
+// Last updated: 02/05/2026, 22:44:43
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+
+        int n = g.size(), m = s.size();
+        int l = 0, r= 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        while(l < m && r < n){
+
+            if(g[r] <= s[l]){
+                r = r + 1;
+            }
+            l = l+1;
+        }
+
+        return r;
+        
+    }
+};
