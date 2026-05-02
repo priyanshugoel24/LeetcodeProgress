afe85d1344a12d57c87d7cf7f8a2b97037dee981
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:33:31 +0530

    Create 3434-FindTheNumberOfDistinctColorsAmongTheBalls

---
+++ 3434-FindTheNumberOfDistinctColorsAmongTheBalls/3434-FindTheNumberOfDistinctColorsAmongTheBalls.cpp
@@ -0,0 +1,27 @@
+// Last updated: 02/05/2026, 22:33:31
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        unordered_map<int, int> ball, color;
+        vector<int> ans;
+        ans.reserve(queries.size());
+        int distinct = 0;
+
+        for(auto &q : queries){
+            int pos = q[0], c = q[1];
+            if(ball.count(pos)){
+                if(--color[ball[pos]] == 0){
+                    color.erase(ball[pos]);
+                    distinct--;
+                }
+            }
+
+            ball[pos] = c;
+            if(++color[c] == 1) distinct++;
+            ans.push_back(distinct);
+        }
+
+        return ans;
+        
+    }
+};
