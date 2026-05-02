1db75aaed36541068a0e89291916e31a3f8ee78d
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:42:10 +0530

    Create 984-MostStonesRemovedWithSameRowOrColumn

---
+++ 984-MostStonesRemovedWithSameRowOrColumn/984-MostStonesRemovedWithSameRowOrColumn.cpp
@@ -0,0 +1,37 @@
+// Last updated: 02/05/2026, 22:42:10
+class Solution {
+public:
+    void dfs(vector<vector<int>>& stones, int idx, vector<bool>& visited){
+
+        int n = stones.size();
+        
+        visited[idx] = true;
+
+        for(int i = 0; i < n; i++){
+
+            int r = stones[idx][0];
+            int c = stones[idx][1];
+
+            if(!visited[i] && ( stones[i][0] == r || stones[i][1] == c) ){
+                dfs(stones, i, visited);
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        
+        int n = stones.size();
+        vector<bool> visited(n, false);
+
+        int components = 0;
+        
+        for(int i = 0; i < n; i++){
+
+            if(!visited[i]){
+                dfs(stones, i, visited);
+                components++;
+            }
+        }
+
+        return n - components;
+    }
+};
