705dbfe134affc38ec53571ccf7f811b67e64618
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:48:25 +0530

    Create 60-PermutationSequence

---
+++ 60-PermutationSequence/60-PermutationSequence.cpp
@@ -0,0 +1,26 @@
+// Last updated: 02/05/2026, 22:48:25
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+         int fact = 1;
+        string res = "";
+        vector<int> arr;
+        for(int i = 1; i <= n; i++)
+        {
+            fact *= i;
+            arr.push_back(i);
+        }
+        
+        //we are using zero based indexing
+        k--;
+        while(arr.size() > 0)
+        {            
+            fact = fact/arr.size();
+            res += to_string(arr[k/fact]);
+            arr.erase(arr.begin() + k/fact);
+            k = k%fact;
+        }
+        return res;
+        
+    }
+};
