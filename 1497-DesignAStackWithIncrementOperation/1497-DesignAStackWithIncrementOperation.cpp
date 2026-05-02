cb555c59b1848c7ce0634da47f33357d9fc33206
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:40:01 +0530

    Create 1497-DesignAStackWithIncrementOperation

---
+++ 1497-DesignAStackWithIncrementOperation/1497-DesignAStackWithIncrementOperation.cpp
@@ -0,0 +1,54 @@
+// Last updated: 02/05/2026, 22:40:01
+class CustomStack {
+public:
+    vector<int> st;
+    vector<int> increments;
+    int n;
+
+    CustomStack(int maxSize) {
+        n = maxSize;
+    }
+    
+    void push(int x) {
+        if(st.size() < n){
+
+            st.push_back(x);
+            increments.push_back(0);
+        }
+    }
+    
+    int pop() {
+        
+        if(st.size() == 0) return -1;
+
+        int idx = st.size() - 1;
+        if(idx > 0){
+
+            increments[idx - 1] += increments[idx]; // lazy propagation
+        }
+
+        int top_val = st[idx] + increments[idx];
+        st.pop_back();
+        increments.pop_back();
+
+        return top_val;
+    }
+    
+    void increment(int k, int val) {
+        
+        int idx = min(k, (int)st.size()) - 1;
+
+        if(idx >= 0){
+
+            increments[idx] += val;
+        }
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
