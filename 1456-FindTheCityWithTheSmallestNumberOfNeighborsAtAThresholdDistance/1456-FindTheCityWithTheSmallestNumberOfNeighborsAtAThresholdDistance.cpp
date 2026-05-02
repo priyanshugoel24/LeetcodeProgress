ee61b4e49ad07732dea768d63c6c048ed99fbfac
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:40:14 +0530

    Create 1456-FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance

---
+++ 1456-FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance/1456-FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.cpp
@@ -0,0 +1,46 @@
+// Last updated: 02/05/2026, 22:40:14
+class Solution {
+private:
+    int dijkstra(int n, int src, vector <pair<int, int>> adj[], int distanceThreshold) {
+        vector <int> dist(n, 1e9);
+        dist[src] = 0;
+        priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+        pq.push({0, src});
+        while (!pq.empty()) {
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            for (auto it : adj[node]) {
+                int adjNode = it.first;
+                int wt = it.second;
+                if (dis + wt < dist[adjNode]) {
+                    dist[adjNode] = dis + wt;
+                    pq.push({dis + wt, adjNode});
+                }
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (dist[i] <= distanceThreshold)
+                cnt++;
+        }
+        return cnt;
+    }    
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector <pair<int, int>> adj[n];
+        for (auto it : edges) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        int city = -1, cntCity = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int cnt = dijkstra(n, i, adj, distanceThreshold);
+            if (cnt <= cntCity) {
+                cntCity = cnt;
+                city = i;
+            }
+        }
+        return city;
+    }
+};
