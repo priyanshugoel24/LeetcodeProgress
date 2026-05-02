29805309c96a51e605c525411c900fd4fd199a26
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:48:11 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.cpp
@@ -0,0 +1,21 @@
+// Last updated: 02/05/2026, 22:48:11
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int red = 0, white = 0, blue = nums.size() - 1;
+
+        while(white <= blue){
+
+            if(nums[white] == 0){
+                swap(nums[white], nums[red]);
+                red++, white++;
+            }else if(nums[white] == 1){
+                white++;
+            }else{
+                swap(nums[white], nums[blue]);
+                blue--;
+            }
+        }
+    }
+};
