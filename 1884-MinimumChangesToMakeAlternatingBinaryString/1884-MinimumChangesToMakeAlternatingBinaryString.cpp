e711c4bb1fe5d2fc0fa34503b79eb20527ce8c61
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:38:32 +0530

    Create 1884-MinimumChangesToMakeAlternatingBinaryString

---
+++ 1884-MinimumChangesToMakeAlternatingBinaryString/1884-MinimumChangesToMakeAlternatingBinaryString.cpp
@@ -0,0 +1,26 @@
+// Last updated: 02/05/2026, 22:38:32
+class Solution {
+public:
+    int minOperations(string s) {
+        
+        int start0 = 0;
+
+        for(int i = 0; i < s.size(); i++){
+
+            if(i % 2 == 0){
+
+                if(s[i] == '1'){
+                    start0++;
+                }
+                
+            }else{
+
+                    if(s[i] == '0'){
+                        start0++;
+                    }
+            }
+        }
+
+        return min(start0, (int)s.size() - start0);
+    }
+};
