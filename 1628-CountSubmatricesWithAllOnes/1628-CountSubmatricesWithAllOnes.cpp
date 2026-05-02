7bb22515681040bae04ff2c2390d288a2b03bc73
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:39:24 +0530

    Create 1628-CountSubmatricesWithAllOnes

---
+++ 1628-CountSubmatricesWithAllOnes/1628-CountSubmatricesWithAllOnes.cpp
@@ -0,0 +1,43 @@
+// Last updated: 02/05/2026, 22:39:23
+class Solution {
+public:
+    int numSubmat(vector<vector<int>>& mat) {
+        
+        int m = mat.size(), n = mat[0].size();
+
+        int result = 0;
+
+        for(int startRow = 0; startRow < m; startRow++){
+
+            vector<int> vec(n, 1);
+            for(int endRow = startRow; endRow < m; endRow++){
+
+                for(int col = 0; col < n; col++){
+                    
+                    vec[col] = vec[col] & mat[endRow][col];
+
+                }
+
+                result += OnedArrayCount(vec);
+            }
+        }
+
+        return result;
+    }
+
+    int OnedArrayCount(vector<int>& vec){
+
+        int cons = 0;
+        int subCount = 0;
+
+        for(int &val : vec){
+
+            if(val == 0) cons = 0;
+            else cons++;
+
+            subCount += cons;
+        }
+
+        return subCount;
+    }
+};
