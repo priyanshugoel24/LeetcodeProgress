b3e7c8b1ceacfdeb5255df0ca027d45cdd796936
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:40:22 +0530

    Create 1437-MinimumInsertionStepsToMakeAStringPalindrome

---
+++ 1437-MinimumInsertionStepsToMakeAStringPalindrome/1437-MinimumInsertionStepsToMakeAStringPalindrome.cpp
@@ -0,0 +1,26 @@
+// Last updated: 02/05/2026, 22:40:22
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.length();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for(int L = 2; L <= n; L++){
+            for(int i = 0; i < n - L + 1; i++){
+
+                int j = i + L - 1;
+
+                if(s[i] == s[j]){ // no insertion needed
+                    dp[i][j] = dp[i+1][j-1];
+                }else{
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
+                }
+
+            }
+        }
+
+        return dp[0][n-1];
+
+    }
+};
