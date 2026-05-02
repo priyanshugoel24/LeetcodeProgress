2c248161894a705314d6696db615ce5b1c785758
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:33:54 +0530

    Create 3309-CountPrefixAndSuffixPairsI

---
+++ 3309-CountPrefixAndSuffixPairsI/3309-CountPrefixAndSuffixPairsI.cpp
@@ -0,0 +1,86 @@
+// Last updated: 02/05/2026, 22:33:53
+struct trieNode{
+    trieNode* children[26];
+    bool isEndofWord;
+};
+
+trieNode* getNode(){
+    trieNode* newNode = new trieNode();
+    newNode->isEndofWord = false;
+
+    for(int i = 0; i<26; i++){
+        newNode->children[i] = NULL;
+    }
+
+    return newNode;
+
+}
+
+class Trie{
+    public:
+    trieNode* root;
+
+    Trie(){
+        root = getNode();
+    }
+
+    void insert(string word){
+        trieNode* pCrawl = root;
+        for(int i = 0; i<word.length(); i++){
+            int idx = word[i] - 'a';
+            if(pCrawl->children[idx] == NULL){
+                pCrawl->children[idx] = getNode();
+            }
+            pCrawl = pCrawl->children[idx];
+        }
+
+        pCrawl->isEndofWord = true;
+    }
+
+    bool searchPrefix(string prefix){
+        trieNode* pCrawl = root;
+
+        for(int i = 0; i<prefix.length(); i++){
+            int idx = prefix[i] - 'a';
+            if(pCrawl->children[idx] == NULL) return false;
+            pCrawl = pCrawl->children[idx];
+        }
+
+        return true;
+    }
+};
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+
+        int n = words.size();
+
+        int count = 0;
+
+        for(int j = 0; j<n; j++){
+            Trie prefixTrie;
+            Trie suffixTrie;
+
+
+            prefixTrie.insert(words[j]);
+            string reversed = words[j];
+            reverse(begin(reversed), end(reversed));
+
+            suffixTrie.insert(reversed);
+
+            for(int i = 0; i<j; i++){
+                if(words[i].length() > words[j].length()) continue;
+
+                string rev = words[i];
+                reverse(begin(rev), end(rev));
+
+                if(prefixTrie.searchPrefix(words[i]) && suffixTrie.searchPrefix(rev)){
+                    count++;
+                }
+            }
+        } 
+
+        return count;
+        
+    }
+};
