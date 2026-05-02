658677f72642556bc006df98133879be35996a9f
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:44:22 +0530

    Create 547-NumberOfProvinces

---
+++ 547-NumberOfProvinces/547-NumberOfProvinces.cpp
@@ -0,0 +1,32 @@
+// Last updated: 02/05/2026, 22:44:22
+class Solution {
+public:
+    void dfs(unordered_map<int,bool>& visited, int src, vector<vector<int>>& isConnected){
+        visited[src] = true;
+        int size = isConnected.size();
+        for(int col = 0; col<isConnected[src].size(); col++){
+            if(src!= col && isConnected[src][col] == 1){
+                if(!visited[col]){
+                    dfs(visited,col,isConnected);
+                }
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+
+        unordered_map<int, bool>visited;
+        int count = 0;
+        int n = isConnected.size();
+
+        for(int i = 0; i<n; i++){
+            if(!visited[i]){
+                dfs(visited, i, isConnected);
+                count++;
+            }
+        }
+
+        return count;
+
+        
+    }
+};
