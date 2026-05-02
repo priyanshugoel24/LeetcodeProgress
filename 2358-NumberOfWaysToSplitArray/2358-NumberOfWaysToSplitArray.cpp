dbcaccbf666d8d3dcaead0f1ae8af88a38d9cdce
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:36:41 +0530

    Create 2358-NumberOfWaysToSplitArray

---
+++ 2358-NumberOfWaysToSplitArray/2358-NumberOfWaysToSplitArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 02/05/2026, 22:36:41
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        const int n = nums.size();
+        long long sum = accumulate(nums.begin(), nums.end(), 0LL);
+        long long acc = 0LL;
+        int cnt = 0;
+
+        for(int i = 0; i<n-1; i++){
+            acc += nums[i];
+            cnt += (2* acc >= sum);
+        }
+
+        return cnt;
+        
+    }
+};
