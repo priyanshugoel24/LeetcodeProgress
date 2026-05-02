90aae6477e25f743ca44612ca0895c7b5df69789
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:46:06 +0530

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
@@ -0,0 +1,41 @@
+// Last updated: 02/05/2026, 22:46:06
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next){
+
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        ListNode* prev = NULL;
+
+        while(slow){
+
+            ListNode* temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while(second){
+
+            if(first->val != second->val){
+                return false;
+            }
+
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
