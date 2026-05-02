22725b4894bdb0d557855c5d48a22f5335dcb2ae
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:33:57 +0530

    Create 3279-AliceAndBobPlayingFlowerGame

---
+++ 3279-AliceAndBobPlayingFlowerGame/3279-AliceAndBobPlayingFlowerGame.cpp
@@ -0,0 +1,10 @@
+// Last updated: 02/05/2026, 22:33:57
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        
+        long long possible_pairs = (long long)m * n/2;
+
+        return possible_pairs;
+    }
+};
