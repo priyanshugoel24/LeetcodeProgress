4d39d60dcb584f2efdbbcbefbf8bd382841d737e
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:41:56 +0530

    Create 1046-MaxConsecutiveOnesIii

---
+++ 1046-MaxConsecutiveOnesIii/1046-MaxConsecutiveOnesIii.cpp
@@ -0,0 +1,41 @@
+// Last updated: 02/05/2026, 22:41:56
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        
+        int size_of_array = nums.size();
+        int flips_allowed = k;
+
+        int longest_ones_length = 0; // max no of consecutive ones with k flips allowed
+
+        // two pointers for traversing the array
+        int left = 0, right = 0;
+
+        while(right < size_of_array){
+            
+            if(nums[right] == 0){
+                flips_allowed--;
+            }
+
+            // check if it is zero and we have used all our flips so shrink the window
+            while (flips_allowed < 0) {
+                if (nums[left] == 0) {
+                    flips_allowed++;    // reclaim a flip when we drop a zero
+                }
+                left++;
+            }
+
+            // get the length of the current valid window
+            int curr_ones_length = right - left + 1;
+
+            // update the max length of the valid window
+            longest_ones_length = max(longest_ones_length, curr_ones_length);
+            
+            // increment the right pointer
+            right++;
+        }
+
+        // return the longest no of consecutive ones in the array
+        return longest_ones_length;
+    }
+};
