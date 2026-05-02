61df870a0937e16d8b10966724071ee3b5054a63
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:32:36 +0530

    Create 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI

---
+++ 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI/3753-MaximumDifferenceBetweenEvenAndOddFrequencyI.cpp
@@ -0,0 +1,19 @@
+// Last updated: 02/05/2026, 22:32:36
+class Solution {
+public:
+    int maxDifference(string s) {
+        unordered_map<char, int> c;
+        for (char ch : s) {
+            ++c[ch];
+        }
+        int maxOdd = 1, minEven = s.size();
+        for (const auto& [_, value] : c) {
+            if (value % 2 == 1) {
+                maxOdd = max(maxOdd, value);
+            } else {
+                minEven = min(minEven, value);
+            }
+        }
+        return maxOdd - minEven;
+    }
+};
