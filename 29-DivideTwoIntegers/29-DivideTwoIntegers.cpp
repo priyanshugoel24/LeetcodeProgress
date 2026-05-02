0c9b430dbdb740bd20d249aa30bd2b741cc76ad6
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:48:56 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -0,0 +1,36 @@
+// Last updated: 02/05/2026, 22:48:56
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+
+        if(dividend == divisor) return 1;
+
+        bool sign = true;
+        if(dividend >= 0 && divisor < 0) sign = false;
+        else if(dividend <= 0 && divisor > 0) sign = false;
+
+        long n = abs(dividend);
+        long d = abs(divisor);
+
+        divisor = abs(divisor);
+        long quotient = 0;
+
+        while(n >= d){
+            int cnt = 0;
+            while( n >= (d << (cnt+1))){
+                cnt += 1;         
+
+            }
+            quotient += 1 << cnt;
+            n -= (d << (cnt));
+        }
+
+        if(quotient == (1 << 31) && !sign) return INT_MIN;
+        if(quotient == (1 << 31) && sign) return INT_MAX;
+
+        return sign? quotient : -quotient;
+
+
+        
+    }
+};
