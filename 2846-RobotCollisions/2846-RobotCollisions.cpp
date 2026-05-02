1ebe8363afead3c06c25dbf5a559f075e573d367
priyanshugoel24 <65210891+priyanshugoel24@users.noreply.github.com>
2026-05-02 22:34:45 +0530

    Create 2846-RobotCollisions

---
+++ 2846-RobotCollisions/2846-RobotCollisions.cpp
@@ -0,0 +1,55 @@
+// Last updated: 02/05/2026, 22:34:45
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        
+        int n = positions.size();
+
+        vector<int> actual_index(n);
+        iota(begin(actual_index), end(actual_index), 0);
+
+        auto lambda = [&](int &i, int &j){
+
+            return positions[i] < positions[j];
+        };
+
+        sort(begin(actual_index), end(actual_index), lambda);
+
+        vector<int> result;
+        stack<int> st;
+
+        for(int &currIdx : actual_index){
+
+            if(directions[currIdx] == 'R'){
+                st.push(currIdx);
+            }else{
+
+                while(!st.empty() && healths[currIdx] > 0){
+                    int top_idx = st.top();
+                    st.pop();
+
+                    if(healths[top_idx] > healths[currIdx]){
+                        healths[top_idx] -= 1;
+                        healths[currIdx] = 0;
+                        st.push(top_idx);
+                    }else if(healths[top_idx] < healths[currIdx]){
+                        healths[currIdx] -= 1;
+                        healths[top_idx] = 0;
+                    }else{
+                        healths[currIdx] = 0;
+                        healths[top_idx] = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+
+            if(healths[i] != 0){
+                result.push_back(healths[i]);
+            }
+        }
+
+        return result;
+    }
+};
